71fc11253582a791305d85b3ef01ef923610fdd9
datluu <tad.louis2810@gmail.com>
2024-04-11 20:08:23 +1000

    remove imgs

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -73,7 +73,7 @@
 	<div class="container">
 		<nav class="navbar navbar-expand-xl" id="navbar">
 			<a href="index.php" class="text-decoration-none mobile-logo">
-				<span class="h2 text-uppercase text-primary bg-dark">Online</span>
+				<span class="h2 text-uppercase text-primary bg-dark">DL</span>
 				<span class="h2 text-uppercase text-white px-2">SHOP</span>
 			</a>
 			<button class="navbar-toggler menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
