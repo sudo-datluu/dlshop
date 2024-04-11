b073fd34ad7b6c81194615cab0a7cd4caab77e33
datluu <tad.louis2810@gmail.com>
2024-04-11 15:05:42 +1000

    add products model to db

---
+++ database/migrations/2024_04_05_100341_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->double('compare_price', 10, 2)->nullable();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('sub_category_id')->constrained('sub_categories')->onDelete('cascade');
+            $table->string('image')->default("")->nullable();
+            $table->integer('qty')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
